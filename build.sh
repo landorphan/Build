c94d7a0e50b89e8b63df67713cf5d55564197305
Timothy Stockstill <timothys@crosslaketech.com>
2020-02-23 18:40:37 -0800

    Initial Checkin

--- build.sh
+++ build.sh
@@ -12,3 +12,16 @@ IFS=$'\n'
 MAX_GROUP=$(echo "${BUILD_GROUPS[*]}" | sort -nr | head -n1)
 
 echo "Found $MAX_GROUP build groups"
+
+
+function RestoreProject() {
+    dotnet restore --no-dependencies "$0"
+}
+
+export -f RestoreProject
+
+# Restore all packages
+for (( i=0; i<=$MAX_GROUP; i++))
+do
+    echo "$BASE_MAP" | grep "|0>$i<0|" | sed -n -E 's/.*\|6>([^<]*)<6.*/\1/p' | xargs -I % bash -c RestoreProject '%'
+done
