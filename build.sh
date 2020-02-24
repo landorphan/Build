2a3f59cf14a8de68e88ce2cb3f9df961d08dfa70
Timothy Stockstill <timothys@crosslaketech.com>
2020-02-23 18:37:04 -0800

    Initial Checkin

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
 echo "Running Build"
+
+INPT=$(cat "$AGENT_BUILDDIRECTORY/info/projects.map")
+HEADER=$(echo "$INPT" | grep "^:")
+BASE_MAP=$(echo "$INPT" | grep "|0>")
+
+BUILD_GROUPS=$(echo "$BASE_MAP" | sed -n -E 's/\|0>([^<]*)<.*/\1/p')
+
+IFS=$'\n'
+MAX_GROUP=$(echo "${BUILD_GROUPS[*]}" | sort -nr | head -n1)
+
+echo "Found $MAX_GROUP build groups"
